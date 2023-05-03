d465aa4211dc558c286dc715584f578eeb11e2fe
filippo-bilardo <filippo.bilardo@tiscali.it>
2023-05-03 11:01:19 +0000

    update

--- Esempi/es1.py
+++ Esempi/es1.py
@@ -2,9 +2,11 @@ import tkinter as tk
 
 root = tk.Tk()
 toolbar = tk.Frame(root)
-open_button = tk.Button(toolbar, text="Open")
-save_button = tk.Button(toolbar, text="Save")
+open_button = tk.Button(toolbar, text="Apri")
+save_button = tk.Button(toolbar, text="Salva")
 open_button.pack(side="left")
 save_button.pack(side="left")
 toolbar.pack(side="top", fill="x")
 root.mainloop()
+
+
